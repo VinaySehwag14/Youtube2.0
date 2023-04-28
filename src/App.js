6c93233b4c85e829b3578cff17b3758c7748619b
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-04-28 15:51:24 +0530

    added search functionality and other component styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import "./App.css";
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  ChannelDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+  VideoDetail,
+} from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ello</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
