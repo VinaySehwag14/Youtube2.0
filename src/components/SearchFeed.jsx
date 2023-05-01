656630d999d8baedcdb158794d71de6a24c74dff
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-05-01 18:47:34 +0530

    fixed uploading bug

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -3,7 +3,7 @@ import { Typography, Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 import { Videos } from "./";
-import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { fetchFromAPI } from "../utils/FetchingApi";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState(null);
