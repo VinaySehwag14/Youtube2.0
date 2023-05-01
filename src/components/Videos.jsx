8d03011d58e78c4254f91515290e425e9e3dd238
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-05-01 13:19:06 +0530

    created channel card and video card with funcitonality

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
 const Videos = ({ videos }) => {
-  console.log(videos, "this is ");
+  return (
+    <Stack
+      direction="row"
+      // direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos?.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
